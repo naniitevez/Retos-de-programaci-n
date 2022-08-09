cc128e97ffc841c2caf85b9c5ba38d3b7a075086
naniitevez <nicole.tevez.97@gmail.com>
2022-08-09 01:15:37 -0300

    ejercico 0  fizz buzz realizado

--- index.ts
+++ index.ts
@@ -17,3 +17,29 @@ appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
  * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
  */
 
+const FizzBuzz = () => {
+  for (let i = 0; i <= 100; i++) {
+    if (i > 0 && i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (i > 0 && i % 3 == 0) {
+      console.log('fizz');
+    } else if (i > 0 && i % 5 == 0) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+  }
+};
+
+// FizzBuzz();
+
+// Reto #1: ¿ES UN ANAGRAMA?
+
+/*
+ * Escribe una función que reciba dos palabras (String) y retorne
+ * verdadero o falso (Bool) según sean o no anagramas.
+ * - Un Anagrama consiste en formar una palabra reordenando TODAS
+ *   las letras de otra palabra inicial.
+ * - NO hace falta comprobar que ambas palabras existan.
+ * - Dos palabras exactamente iguales no son anagrama.
+ */
